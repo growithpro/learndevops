45a4c3dda4e984a391fd10e94f882d2007589d2a
Naveen Joshi <naveen@GROWITHPRO.>
2026-04-14 15:39:49 +0530

    Add Express App

--- learngit/app.js
+++ learngit/app.js
@@ -1,1 +1,12 @@
-console.log("This Will Be Printed ")
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello! Ye app Render pe live hai 🚀');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
